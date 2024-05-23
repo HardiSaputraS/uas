4b31b806013629a09060c6a395c49ba47b32abca
Hardy <125684866+HardiSaputraS@users.noreply.github.com>
2024-05-23 09:49:15 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$nama = "hardi saputra";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?= $nama ?>
+    </p>
+</body>
+</html>
